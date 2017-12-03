3ba7c4b3421d89bc76553c1a00a8044fb1880dcb
100297 <league@WTS7.attlocal.net>
2017-12-03 11:27:30 -0800

    TextUndoRedo
    Use Stack to pop on and off letters

--- src/IntroToStacks/TextUndoRedo.java
+++ src/IntroToStacks/TextUndoRedo.java
@@ -1,13 +1,31 @@
 package IntroToStacks;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class TextUndoRedo {
+public class TextUndoRedo implements KeyListener {
 	JFrame start = new JFrame();
 	JPanel next = new JPanel();
 	JLabel after = new JLabel();
+	public static void main(String[] args) {
+		TextUndoRedo tur = new TextUndoRedo();
+		Stack<String> key = new Stack<String>();
+	
+	}
+	TextUndoRedo(){
+		
+	start.setSize(400, 250);	
+	start.setVisible(true);
+	start.add(next);
+	next.add(after);
+	start.addKeyListener(this);
+	
+	}
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
 
@@ -20,5 +38,32 @@ public class TextUndoRedo {
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+	//	after.add(e.getKeyChar());
+				 
+			 String Before = after.getText();
+				after.setText(Before + e.getKeyChar());
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+	/*	if (next == e.getSource()) {
+		after.getText();
+		after.setText(after.getText()+ "");
+		System.out.println("test" + after);
+		}*/
+		
+	}
+	
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+			System.out.println("test");
+		}
+	}
 	
 }
